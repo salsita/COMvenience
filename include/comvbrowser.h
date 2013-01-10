5f0e3314ffd327059cfa22cb7409b2e96f071ae9
IUnknown68 <arne@salsitasoft.com>
2013-01-10 18:43:46 +0100

    Added a safe pointer specialization ConvDispPtr<IWebBrowser2>
    Signed-off-by: IUnknown68 <arne@salsitasoft.com>

--- include/comvbrowser.h
+++ include/comvbrowser.h
@@ -11,5 +11,74 @@
 
 #pragma once
 
-#include <atlcomcli.h>
+#include <comvptrs.h>
+#include <ExDispid.h>
+#include <_implementQIPtr.h>
 
+/*==============================================================================
+ * namespace
+ */
+namespace COMvenience
+{
+
+/*==============================================================================
+ * class WebBrowser2Ptr
+ *
+ * specialization for IWebBrowser2
+ */
+template<> class ConvDispPtr<IWebBrowser2> :
+      public CComQIPtr<IWebBrowser2>,
+      public DispatchConvenience<ConvDispPtr<IWebBrowser2> > {
+public:
+  IMPLEMENT_QI_PTR(ConvDispPtr, IWebBrowser2)
+
+  _Check_return_ CComPtr<IHTMLDocument2> getDocument(_Out_opt_ HRESULT * aResultPtr = NULL) {
+    HRESULT hr = E_INVALIDARG;
+    CComPtr<IHTMLDocument2> document;
+    ATLASSERT(p);
+    if (p) {
+      CComPtr<IDispatch> dispatch;
+      hr = p->get_Document(&dispatch);
+      if (SUCCEEDED(hr)) {
+        hr = dispatch.QueryInterface(&document.p);
+      }
+    }
+    if (aResultPtr) {
+      (*aResultPtr) = hr;
+    }
+    return document;
+  }
+
+  _Check_return_ CComPtr<IHTMLWindow2> getWindow(_Out_opt_ HRESULT * aResultPtr = NULL) {
+    HRESULT hr = E_INVALIDARG;
+    CComPtr<IHTMLWindow2> window;
+    CComQIPtr<IHTMLDocument2> document(getDocument(&hr));
+    if (document) {
+      hr = document->get_parentWindow(&window);
+    }
+    if (aResultPtr) {
+      (*aResultPtr) = hr;
+    }
+    return window;
+  }
+
+  _Check_return_ ConvIDispatchExPtr getScriptDispatchEx(_Out_opt_ HRESULT * aResultPtr = NULL) {
+    return ConvIDispatchExPtr(getWindow(aResultPtr));
+  }
+
+  READYSTATE getReadyState(_Out_opt_ HRESULT * aResultPtr = NULL) {
+    READYSTATE ret = READYSTATE_UNINITIALIZED;
+    HRESULT hr = (*this)->get_ReadyState(&ret);
+    if (aResultPtr) {
+      (*aResultPtr) = hr;
+    }
+    return ret;
+  }
+      
+};
+typedef ConvDispPtr<IWebBrowser2> WebBrowser2Ptr;
+
+// cleanup
+#undef IMPLEMENT_QI_PTR
+
+} // namespace COMvenience
